d60aaa322e3cd95c21e34499f1e8dc0d8a4f2459
Dipak Khade <dipakhade214@gmail.com>
2025-04-07 21:03:20 +0530

    added webhook for testing

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -1,7 +1,7 @@
+import { NextResponse } from "next/server";
 
 export async function POST(req) {
-  console.log('this is req----------', req)
-
+  console.log("this is req----------", req);
 
   return NextResponse.json(
     { massage: "message sent succesfully" },
